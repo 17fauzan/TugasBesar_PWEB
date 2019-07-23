c6d7f07f3bfd86536556e11b7f26c384ae3ad3a9
helcayogak <helcayogak@gmail.com>
2019-07-23 21:18:47 +0700

    helcute

--- models/order.js
+++ models/order.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = require('../Configs/Sequelize');
+const sequelize = require('../configs/sequelize');
 
 class Order extends Sequelize.Model {}
 
